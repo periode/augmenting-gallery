45b738378ab62917ba79010a8cee8d921ab45f63
Pierre Depaz <pierre.depaz@gmail.com>
2019-02-05 01:37:41 +0100

    added scaffolding 1

---
+++ code/01_Augmented_Scaffolding_ObjectStory/Assets/Scripts/CameraGrabber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraGrabber : MonoBehaviour {
+
+    public RawImage cameraSurface;
+    WebCamTexture mCam;
+
+	// Use this for initialization
+	void Start () {
+        mCam = new WebCamTexture(640, 480);
+        cameraSurface.material.mainTexture = mCam;
+        cameraSurface.texture = mCam;
+        mCam.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
