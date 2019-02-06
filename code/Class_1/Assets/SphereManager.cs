8aa4e3c910f147b8188c713cf7385b3aebf3f110
Pierre Depaz <pierre.depaz@gmail.com>
2019-02-06 18:22:52 +0100

    added code

---
+++ code/Class_1/Assets/SphereManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
