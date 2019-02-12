f3735c371bc2d3fa292ed5ba4faf73d5f649d952
Pierre Depaz <pierre.depaz@gmail.com>
2019-02-12 18:39:25 +0100

    added scaffolding 2

---
+++ code/02_Augmented_Scaffolding_Mobile/Assets/ArtefactManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtefactManager : MonoBehaviour {
+
+    public GameObject artefact;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ReceivedTouch(Vector3 position, Quaternion rotation){
+        GameObject.Instantiate(artefact, position, rotation);
+    }
+}
