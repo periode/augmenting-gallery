7152da49714e44b2f61ae701c0914c3ade4c9cef
Pierre Depaz <pierre.depaz@gmail.com>
2025-12-02 08:48:28 +0100

    Delete code/summer_2023 directory

--- code/summer_2023/react/src/Model.js
+++
@@ -1,9 +0,0 @@
-import { useGLTF } from "@react-three/drei";
-
-function Model(props) {
-    // const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/ruby/model.gltf')
-    const { scene } = useGLTF('/models/Wood_Tower.glb')
-    return <primitive object={scene} {...props} />
-}
-
-export default Model;
